21230e36172fd9d7bffab4916f1756ab5b5e4f72
dyonende <32299874+dyonende@users.noreply.github.com>
2020-05-17 17:25:11 +0200

    Delete checksentiment.py

--- checksentiment.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-cmd = "curl -d \"text=" + sys.argv[1] + "\" http://text-processing.com/api/sentiment/"
-# returns output as byte string
-returned_output = os.system(cmd)
-
-print(returned_output)
